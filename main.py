136266b67b8b5801c12bc6291035717cc765d29b
Sasank Madineni <sasank1020@gmail.com>
2020-06-26 23:07:34 -0700

    added WPalettes

--- main.py
+++ main.py
@@ -22,31 +22,60 @@ class Palette:
             print(f"Opening file \"{file.name}\"")
             content = file.read().splitlines()
         for color in content:
-            
-            self.colors.append(color)
+            color_arr = color.split(' ')
+            self.colors.append((int(color_arr[0]), int(color_arr[1]), int(color_arr[2])))
         file.close()
-        print(f"loaded palette with colors: f{self.colors}")
+        print(f"loaded palette with colors: {self.colors}")
         return
 
     def random_color(self):
         '''returns a random color from the current palette'''
-        return self.colors[random.randint(0, len(self.colors))]
+        return self.colors[random.randint(0, len(self.colors) - 1)]
+
+class WPalette(Palette):
+    '''Weighted Palette Class'''
+
+    def __init__(self, name, colors=[]):
+        '''Palette Constructor'''
+        self.name = name
+        self.colors = colors
+
+    def load_palette_from_txt(self, filename):
+        '''loads a weighted palette from a text file'''
+        with open(filename, 'r') as file:
+            print(f"Opening file \"{file.name}\"")
+            content = file.read().splitlines()
+        for color in content:
+            color_arr = color.split(' ')
+            self.colors.append(((int(color_arr[0]), int(color_arr[1]), int(color_arr[2])), float(color_arr[3])))
+        file.close()
+        print(f"loaded palette with colors: {self.colors}")
+        return
+
+    def random_color(self):
+        '''returns a random color from the current palette'''
+        return self.colors[random.randint(0, len(self.colors) - 1)]
 
 def open_image(path):
     '''returns image from <path>'''
     return Image.open(path)
 
-def generate_random_image(palette):
+def generate_random_image(size, palette):
     '''randomizes the pixels in the image with colors from the palette'''
-    image = Image.new("RGB", (16, 16))
-    for x in range(16):
-        for y in range(16):
-            image[x][y] = palette.random_color()
+    image = Image.new("RGB", size)
+    i, j = size
+    for x in range(i):
+        for y in range(j):
+            image.putpixel((x, y), palette.random_color())
     image.show()
+    return
+
+def generate_markov_image(size, palette):
+    '''markov chain image generator using only colors from the palette'''
     return
 
 if __name__ == "__main__":
 
-    test_pal = Palette("Test Palette")
+    test_pal = WPalette("Test Palette")
     test_pal.load_palette_from_txt("test.txt")
-    generate_random_image(test_pal)
+    generate_random_image((200, 200), test_pal)
